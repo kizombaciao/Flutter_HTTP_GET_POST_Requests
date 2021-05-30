a73a4df2ed42d3c8de25131734d0dadcdf0c8691
Kizomba <kizomba@github.com>
2021-05-30 21:17:28 +0200

    1

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+var _postsJson = [];
+
+class _MyAppState extends State<MyApp> {
+  final url = "https://jsonplaceholder.typicode.com/posts";
+
+  void fetchPosts() async {
+    try {
+      final response = await get(Uri.parse(url));
+      final jsonData = jsonDecode(response.body) as List;
+      setState(() {
+        _postsJson = jsonData;
+      });
+    } catch (err) {}
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+            itemCount: _postsJson.length,
+            itemBuilder: (context, i) {
+              final post = _postsJson[i];
+              return Text(
+                  "Title: ${post["title"]}\n Body: ${post["body"]}\n\n");
+            }),
+      ),
+    );
+  }
+}
+
+
+'''
+https://youtu.be/ljTkGz_O36I
+'''
